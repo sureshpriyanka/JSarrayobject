0ffe921b07e5894e1948b75032122086c5130476
sureshpriyanka <sureshpriyanka@users.noreply.github.com>
2017-01-11 14:46:39 +0530

    Delete app.js

--- app.js
+++
@@ -1,3 +0,0 @@
-var employee=[{name:"PRIYA",Age:20,salary:"Rs.150000",address:{city:"Tirupur",state:"Tamilnadu",pincode:604602}}, {name:"SUPRIYA",Age:20,salary:"Rs.152000",address:{city:"Tirupur",state:"Tamilnadu",pincode:604602}},{name:"SRI PRIYA",Age:20,salary:"Rs.51000",address:{city:"Tirupur",state:"Tamilnadu",pincode:604602}},{name:"ARUN",Age:20,salary:"Rs.150000",address:{city:"Tirupur",state:"Tamilnadu",pincode:604602}},{name:"KRISH",Age:20,salary:"Rs.150000",address:{city:"Tirupur",state:"Tamilnadu",pincode:604602}}];
-
-console.log("NAME:"+employee[3].name+"\nAGE:"+employee[3].Age+"\nsalary:"+employee[3].salary+"\nCITY:"+employee[3].address.city+"\nstate:"+employee[3].address.state+"\npincode:"+employee[3].address.pincode);
